ef96b7df6b4457a627553a1d501afa141ac7e5f7
GiorgTsial <31443883+GiorgTsial@users.noreply.github.com>
2021-05-24 09:58:35 +0100

    Update fit_SFEM_on_nonlinear.py

--- fit_SFEM_on_nonlinear.py
+++ fit_SFEM_on_nonlinear.py
@@ -23,7 +23,7 @@ def KL_distance(real_samples, generated_samples, bandwidth, lower_plot_val, uppe
     return kl_diverg
 
 
-training_keys = [1.0, 10.0, 22.0, 31.0, 40.0]
+training_keys = [1.0, 4.0, 7.0, 10.0, 13.0, 16.0, 19.0, 22.0, 25.0, 28.0, 31.0, 34.0, 37.0, 40.0]
 validation_keys = [2.0, 5.0, 8.0, 11.0, 14.0, 17.0, 20.0, 23.0, 26.0, 29.0, 32.0, 35.0, 38.0]
 testing_keys = [3.0, 6.0, 9.0, 12.0, 15.0, 18.0, 21.0, 24.0, 27.0, 30.0, 33.0, 36.0, 39.0]
 
